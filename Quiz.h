9bdcdd5842fbb3042b8154869f0c18099341350d
khoicrtp <60875449+khoicrtp@users.noreply.github.com>
2020-08-30 20:27:14 +0700

    Update Quiz.h

--- Quiz.h
+++ Quiz.h
@@ -5,29 +5,31 @@
 #include "Player.h"
 #include "Question.h"
 
-int checkIdentity(string username, string password, Player *&plrs,
-                  int NumPlayer, int &pos);
+int checkIdentity(string username, string password, Player*& plrs,
+	int NumPlayer, int& pos);
 void login();
 
 inline void showAdministratorMenu() {
-  int choice;
+	int choice = 0;
 
-  cout << "ADMINISTRATOR MENU: " << endl;
-  cout << "1. Change password" << endl;
-  cout << "2. Edit user's info " << endl;
-  cout << "3. Edit question list" << endl;
+	cout << "ADMINISTRATOR MENU: " << endl;
+	cout << "1. Change password" << endl;
+	cout << "2. Edit user's info " << endl;
+	cout << "3. Edit question list" << endl;
 
-  cin >> choice;
+	cin >> choice;
 
-  if (choice == 1) {
-    editPlayer("Administrators.txt");
-  } else if (choice == 2) {
-    editPlayer("Users.txt");
-  } else if (choice == 3) {
-    vector<Question> a;
-    loadQuestionList("QuestionList.txt", a);
-    editQuestionList(a);
-  }
+	if (choice == 1) {
+		editPlayer("Administrators.txt");
+	}
+	else if (choice == 2) {
+		editPlayer("Users.txt");
+	}
+	else if (choice == 3) {
+		vector<Question> a;
+		loadQuestionList("QuestionList.txt", a);
+		editQuestionList(a);
+	}
 }
 
 #endif // !_QUIZ_H_
