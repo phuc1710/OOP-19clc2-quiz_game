d624627fe09fa53591c20259c0947298ecd72046
khoicrtp <60875449+khoicrtp@users.noreply.github.com>
2020-09-20 16:42:57 +0700

    fix

--- main.cpp
+++ main.cpp
@@ -2,22 +2,7 @@
 
 int main()
 {
-    /*Player* plrs = new Player;
-    plrs->setUserName("name");
-    plrs->setPassword("pass");
-    savePlayerList(FilePath, plrs, 1);*/
     login();
-    //createTopic();
-    //Question q;
-    /*vector<Question> Q;
-    loadQuestionList(QUESTION_PATH_DATA, Q);
-    cout << "t " << Q.size() << endl;
-    for (int i = 0; i < Q.size(); i++)
-        Q[i].print();*/
-    /*vector<Player> p;
-    loadPlayerList(PLAYER_PATH_DATA, p);
-    cout << p.size() << endl;
-    for (int i = 0; i < p.size(); i++)
-        cout << p[i].getUserName() << endl;*/
+    
     return 0;
 }
